a45a667e84996db32e4980ba97fc0745b8ecbfd6
Dinakm <dinakm1998@gmail.com>
2023-05-31 23:53:44 +0300

    add a function to format a joke

--- script.js
+++ script.js
@@ -7,33 +7,20 @@ function generateRandomNumber(num) {
   const KnockerJokes = [['Beets','Beats me!'], ['Lettuce', 'Lettuce in.'],['Hutch','Bless you!'] ,['Spell','W-h-o.']];
   // Store the 'wisdom' in an array
   let fullJoke = [];
-  /*
-  // Iterate over the object
-  for(let prop in knockJokes) {
-    let optionIdx = generateRandomNumber(knockJokes[prop].length)
-    
-    // use the object's properties to customize the message being added to personalWisdom  
-    switch(prop) {
-      case 'signInfo':
-        personalWisdom.push(`Your sign right now is a "${collectiveWisdom[prop][optionIdx]}".`)
-        break
-      case 'fortuneOutput':
-        personalWisdom.push(`You are having: "${collectiveWisdom[prop][optionIdx]}".`)
-        break
-      case 'advice':
-        personalWisdom.push(`You should: "${collectiveWisdom[prop][optionIdx]}".`)
-        break
-      default:
-        personalWisdom.push('There is not enough info.')
-    }
-  }
-  
-  function formatWisdom(wisdom) {
+
+    let optionIdx = generateRandomNumber(KnockerJokes.length);
+    fullJoke.push('knock knock');
+    fullJoke.push('who is there');
+    fullJoke.push(KnockerJokes[optionIdx][0]);
+    fullJoke.push(KnockerJokes[optionIdx][0] + ' who');
+    fullJoke.push(KnockerJokes[optionIdx][1]);
+
+  function formatjoke(fullJoke) {
     // Add some ASCII here?
     // Add in more symbols to the array? like emojies or what not?
-    const formatted = personalWisdom.join('\n')
+    const formatted = fullJoke.join('\n')
     console.log(formatted)
   }
   
-  formatWisdom(personalWisdom);
-  */
+  formatjoke(fullJoke);
+  
